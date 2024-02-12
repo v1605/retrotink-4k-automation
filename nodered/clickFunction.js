5c536486fe454361054599befe3dfd274c4af7f0
v1605 <55302877+v1605@users.noreply.github.com>
2024-02-12 14:09:39 -0500

    Added missing awaits and updated timings

--- nodered/clickFunction.js
+++ nodered/clickFunction.js
@@ -5,10 +5,10 @@ for(let click of clickArray){
     body[click.dir === "up" ? 0 : 1] = {};
     for(let i=0; i < click.clicks; i++){
         node.send(body);
-        delay(500);
+        await delay(500);
     }
+    await delay(800);
     node.send([null, null, {}, null])
     await delay(500)
 }
-//return msg;
 return [null, null, null, {}];
